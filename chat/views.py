44416279b590b0b40b1ebb27e9e036dc33f1ef10
Maylon Pedroso <maylonpedroso@gmail.com>
2019-11-06 22:27:33 -0500

    Convert entered chat room names to a valid slug

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.utils.text import slugify
 
 from chat.models import ChatMessage, ChatRoom
 
@@ -7,6 +9,11 @@ from chat.models import ChatMessage, ChatRoom
 @login_required(login_url='/user/login')
 def room_view(request):
     room_name = request.GET.get('room_name', 'global')
+    room_slug = slugify(room_name)
+
+    if room_name != room_slug:
+        return redirect(reverse('chat:index') + f'?room_name={room_slug}')
+
     room, created = ChatRoom.objects.get_or_create(name=room_name)
     messages = ChatMessage.objects.filter(room=room.pk).order_by('-created_at')[:50]
     return render(request, 'chat/room.html', {
