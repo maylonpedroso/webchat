13c908e1cb71f617eb67f7369d880684ad0e1ded
Maylon Pedroso <maylonpedroso@gmail.com>
2020-08-06 22:47:43 -0500

    Setup github action build job for pull request (#4)

--- bots/__init__.py
+++ bots/__init__.py
@@ -1,4 +1,4 @@
 from .stock import stock_bot
 from .register import active_bots
 
-__all__ = ('active_bots',)
+__all__ = ('active_bots', 'stock_bot')
--- bots/stock.py
+++ bots/stock.py
@@ -50,4 +50,3 @@ def stock_message(message):
         'datetime': datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
         'username': USERNAME,
     }
-
